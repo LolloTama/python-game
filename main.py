15a3804c05b078a86c0bd5334c50f057dad3025a
studente <studente@liceospalla.local>
2019-02-27 09:23:27 +0100

    aggiunto world

--- main.py
+++ main.py
@@ -15,3 +15,20 @@ for y in range(10):
       print("[ ]", end="")
 
   print()
+
+
+class World:
+  def __init__(self,level_number,larghezza,altezza):
+    self.level_number = level_number
+    self.larghezza = larghezza
+    self.altezza = altezza
+
+  def create(self,larghezza,altezza):
+    for y in range(10):
+      for x in range(10):
+        if e.x == x and e.y == y:
+          print("[{}]".format(e.graphic), end="")
+        else:
+          print("[ ]", end="")
+
+      print()
